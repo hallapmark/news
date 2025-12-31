ffdc61286cd31f0786ebe0cb98139de23b5c3dda
Mark Hallap <hallapmark@gmail.com>
2025-12-31 12:14:18 +0200

    Add error validation to news creation

--- src/pages/LisaUudis.jsx
+++ src/pages/LisaUudis.jsx
@@ -1,10 +1,16 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { toast } from "react-toastify";
 
 function LisaUudis() {
   const uudiseRef = useRef(null);
+  const [sonum, uuendaSonum] = useState("");
 
   const lisaUusUudis = () => {
+    if (sonum) {
+      toast.error("Paranda enne sistestamist vead")
+      return;
+    }
+
     let uudised = JSON.parse(localStorage.getItem("uudised")) || [];
     uudised.push(uudiseRef.current.value);
     uudised = JSON.stringify(uudised);
@@ -12,11 +18,24 @@ function LisaUudis() {
     toast.success("Uudis lisatud!");
   }
 
+  const kontrolli = () => {
+    uuendaSonum("");
+
+    if (uudiseRef.current.value.charAt(0) === uudiseRef.current.value.charAt(0).toLowerCase()) {
+      uuendaSonum("Sisestasid uudise väikse tähega, palun paranda!")
+    }
+
+    if (uudiseRef.current.value.includes("  ")) {
+      uuendaSonum("Sisestasid kaks tühikut, palun paranda!")
+    }
+  }
+
   return (
     <div style={{ margin: "20px 8px"}}>
+      <div>{sonum}</div>
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "12px" }}>
         <label className="field-label">Uudise nimi</label>
-        <input ref={uudiseRef} />
+        <input onChange={kontrolli} ref={uudiseRef} />
         <button onClick={lisaUusUudis}>Lisa uudis</button>
       </div>
     </div>
